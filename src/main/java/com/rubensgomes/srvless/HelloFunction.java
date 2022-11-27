eaf26d21a189b6cdf05f81af88ce174a2f1836c4
Rubens Gomes <rubens.gomes@aa.com>
2022-11-27 14:53:18 +0000

    updates

---
+++ src/main/java/com/rubensgomes/srvless/HelloFunction.java
@@ -0,0 +1,23 @@
+package com.rubensgomes.srvless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * The application start class.
+ * 
+ * @author Rubens Gomes
+ */
+@Slf4j
+@SpringBootApplication
+public class HelloFunction
+{
+   public static void main( String[] args )
+   {
+      log.debug( "Starting the [{}] function",
+                 HelloFunction.class.getName() );
+      SpringApplication.run( HelloFunction.class );
+   }
+}
---
+++ src/main/java/com/rubensgomes/srvless/web/controller/HelloRestController.java
@@ -0,0 +1,45 @@
+package com.rubensgomes.srvless.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * {@link HelloRestController} is a simple hello RESTful API.
+ *
+ * @author Gomes, Rubens <Rubens.S.Gomes@gmail.com>
+ */
+@Slf4j
+@RestController
+public class HelloRestController
+{
+   static final String OPERATION = "/api/hello";
+
+   /**
+    * The hello operation.
+    *
+    * @param name some text to display back.
+    * @return a hello text message.
+    */
+   //@formatter:off
+   @GetMapping
+   (
+      path     = { OPERATION                        },
+      consumes = { MediaType.APPLICATION_JSON_VALUE },
+      produces = { MediaType.APPLICATION_JSON_VALUE }
+   )
+   //@formatter:on
+   @ResponseStatus( code = HttpStatus.OK )
+   public String hello( @RequestParam( value = "name",
+                                       defaultValue = "World" ) String name )
+   {
+      log.trace( "Processing hello" );
+      return String.format( "Hello %s!",
+                            name );
+   }
+}
